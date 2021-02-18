a37f151a3fcb50d2c0b55d8767ef9cbfe5a07651
Wilson Almeida <wmeida@gmail.com>
2021-02-18 20:15:39 -0300

    pid library

--- src/components/brassagem/Panela.js
+++ src/components/brassagem/Panela.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Row, Col, Button, Modal, Slider, Badge } from 'antd';
+import { Row, Col, Button, Modal, Slider, Badge, Tag } from 'antd';
 import { Statistic, Card, Progress, Divider } from 'antd';
 
 import {
@@ -7,9 +7,6 @@ import {
   EllipsisOutlined,
   SettingOutlined,
 } from '@ant-design/icons';
-
-
-
 
 import { GlobalContext } from '../../context/GlobalState';
 
@@ -36,13 +33,11 @@ function Panela({ panela }) {
   };
 
   const marks2 = {
-
     50: '50C',
     63: '63C',
     67: '67C',
     76: '76C',
-    100: '100C'
-
+    100: '100C',
   };
 
   const marks3 = {
@@ -51,7 +46,7 @@ function Panela({ panela }) {
     60: '60min',
     90: '90min',
     120: '120min',
-  }
+  };
   const showModal = () => {
     setIsModalVisible(true);
   };
@@ -60,30 +55,28 @@ function Panela({ panela }) {
   };
 
   const handlePotencia = () => {
-    alterarPotenciaPanela(panela.id, potenciaDesejada)
+    alterarPotenciaPanela(panela.id, potenciaDesejada);
     setIsModalVisible(false);
-    console.log(potenciaDesejada)
-
+    console.log(potenciaDesejada);
   };
 
   const handleTemperatura = () => {
-    alterarTemperaturaPanela(panela.id, temperaturaDesejada, tempoDesejado)
+    alterarTemperaturaPanela(panela.id, temperaturaDesejada, tempoDesejado);
     setIsModalTempVisible(false);
-    console.log(potenciaDesejada)
-
+    console.log(potenciaDesejada);
   };
 
   const onChange = (value) => {
-    setPotenciaDesejada(value)
-  }
+    setPotenciaDesejada(value);
+  };
 
   const onChangeTemp = (value) => {
-    setTemperaturaDesejada(value)
-  }
+    setTemperaturaDesejada(value);
+  };
 
   const onChangeTempo = (value) => {
-    setTempoDesejado(value)
-  }
+    setTempoDesejado(value);
+  };
 
   const handleCancel = () => {
     setIsModalVisible(false);
@@ -95,8 +88,9 @@ function Panela({ panela }) {
 
   //console.log(panela)
 
-
-  const { alterarPotenciaPanela, alterarTemperaturaPanela } = useContext(GlobalContext);
+  const { alterarPotenciaPanela, alterarTemperaturaPanela } = useContext(
+    GlobalContext
+  );
 
   return (
     <Col xs={24} lg={6} style={style}>
@@ -104,59 +98,87 @@ function Panela({ panela }) {
         title={panela.nome}
         bordered
         extra={<Button type="primary">Ligar</Button>}
-        style={{ backgroundColor: '#fff', border: 2, borderStyle: 'solid', borderWidth: 3, borderColor: '#000' }}
+        style={{
+          backgroundColor: '#fff',
+          border: 2,
+          borderStyle: 'solid',
+          borderWidth: 3,
+          borderColor: '#000',
+        }}
         actions={[
           <SettingOutlined key="setting" />,
           <EditOutlined key="edit" />,
           <EllipsisOutlined key="ellipsis" />,
         ]}
       >
-
-
         <Row justify="center">
           <Col span={12}>
             <Statistic
-
               title="Temperatura"
               value={panela.temperatura}
               precision={2}
               valueStyle={{ color: '#3f8600' }}
               suffix="C"
             />
-            <Button onClick={showModalTemp}>
-              <Badge >
-                <span>{panela.temperatura}</span>
-              </Badge>
-            </Button>
-            <small>{panela.tempdesejada}</small>
-            <small>{panela.tempodesejada} segundos</small>
+            <Tag color="processing" onClick={showModalTemp}>
+              SP: {panela.tempdesejada}C
+            </Tag>
           </Col>
 
           <Col span={12}>
-            <Progress type="circle" percent={panela.potencia} width={80} onClick={showModal}>
+            <Progress
+              type="circle"
+              percent={panela.potencia}
+              width={80}
+              onClick={showModal}
+            >
               Potencia
             </Progress>
           </Col>
         </Row>
         <Divider />
+        <Col span={12}>
+          <Statistic title="Tempo" value={panela.tempodesejado} precision={0} />
+          <Button style={{ marginTop: 16 }} type="primary">
+            Adicionar
+          </Button>
+        </Col>
       </Card>
-      <Modal title="Alterar potencia" visible={isModalVisible} onOk={handlePotencia} onCancel={handleCancel} onAuto={handleTemperatura}>
-        <Slider marks={marks} step={1} defaultValue={potenciaDesejada} onChange={onChange} />
-
-
+      <Modal
+        title="Alterar potencia"
+        visible={isModalVisible}
+        onOk={handlePotencia}
+        onCancel={handleCancel}
+        onAuto={handleTemperatura}
+      >
+        <Slider
+          marks={marks}
+          step={1}
+          defaultValue={potenciaDesejada}
+          onChange={onChange}
+        />
       </Modal>
 
-      <Modal title="Alterar Temperatura" visible={isModalTempVisible} onOk={handleTemperatura} onCancel={handleCancelTemp} >
-
-        <Slider marks={marks2} step={1} defaultValue={temperaturaDesejada} onChange={onChangeTemp} />
-        <Slider marks={marks3} step={1} defaultValue={tempoDesejado} onChange={onChangeTempo} />
-
+      <Modal
+        title="Alterar Temperatura"
+        visible={isModalTempVisible}
+        onOk={handleTemperatura}
+        onCancel={handleCancelTemp}
+      >
+        <Slider
+          marks={marks2}
+          step={1}
+          defaultValue={temperaturaDesejada}
+          onChange={onChangeTemp}
+        />
+        <Slider
+          marks={marks3}
+          step={1}
+          defaultValue={tempoDesejado}
+          onChange={onChangeTempo}
+        />
       </Modal>
-
-
     </Col>
-
-
   );
 }
 
